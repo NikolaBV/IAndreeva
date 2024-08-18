461efa12de63f012f75b0cc0dfd5f107b750271f
NikolaBV <bulgarianmapper@gmail.com>
2024-08-18 13:56:13 +0300

    Added get by id method

--- Project/API/Controllers/PostsController.cs
+++ Project/API/Controllers/PostsController.cs
@@ -9,21 +9,15 @@ namespace API.Controllers
 {
     public class PostsController : BaseAPIController
     {
-        private readonly IMediator _mediator;
-        public PostsController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet]
         public async Task<ActionResult<List<Post>>> GetPosts()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Post>> GetPost(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query { Id = id });
         }
     }
 }
