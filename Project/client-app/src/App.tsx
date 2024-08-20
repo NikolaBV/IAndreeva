167c840b815d33ff7af2d80e4f2d047da3740cb2
NikolaBV <bulgarianmapper@gmail.com>
2024-08-20 16:15:07 +0300

    Cleaned up components and file structure

--- Project/client-app/src/App.tsx
+++ Project/client-app/src/App.tsx
@@ -1,40 +1,9 @@
-import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
-import "./styles/index.css";
-import Post from "./PostCard";
-import { Col } from "antd";
-import { PostModel } from "./api/models";
+import Posts from "./pages/Posts/Home";
 
 export default function App() {
-  const postsQuery = useQuery({
-    queryKey: ["posts"],
-    queryFn: () => axios.get("http://localhost:5000/api/posts/"),
-  });
-
-  if (postsQuery.isLoading) {
-    return <div>Loading...</div>;
-  }
   return (
     <>
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <Col
-          style={{
-            height: "100vh",
-            width: "70%",
-            marginRight: "0.5rem",
-          }}
-        >
-          {postsQuery.data?.data?.map((post: PostModel) => (
-            <Post
-              id={post.id}
-              title={post.title}
-              description={post.description}
-              createdAt={new Date(post.createdAt)}
-            ></Post>
-          ))}
-        </Col>
-        <Col style={{ width: "30%", height: "100vh" }}></Col>
-      </div>
+      <Posts></Posts>
     </>
   );
 }
