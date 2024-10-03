58246faaceb2e05c8ab820b42893c362905bfa7f
NikolaBV <bulgarianmapper@gmail.com>
2024-10-03 08:46:57 +0300

    Added pagination on the server

--- Project/client-app/src/api/models.ts
+++ Project/client-app/src/api/models.ts
@@ -38,3 +38,20 @@ export interface RegisterModel {
   displayName: string;
   userName: string;
 }
+
+export interface Pagination {
+  currentPage: number;
+  itemsPerPage: number;
+  totalItems: number;
+  totalPages: number;
+}
+
+export class PaginatedResult<T> {
+  data: T;
+  pagination: Pagination;
+
+  constructor(data: T, pagination: Pagination) {
+    this.data = data;
+    this.pagination = pagination;
+  }
+}
